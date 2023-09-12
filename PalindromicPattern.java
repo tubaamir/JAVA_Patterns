a493f83657e8224aa595a5af844a6bdc06609bc6
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 09:41:16 +0530

    Update PalindromicPattern.java

--- PalindromicPattern.java
+++ PalindromicPattern.java
@@ -1,4 +1,5 @@
-//Printing Pattern Number Pyramid
+//Printing Pattern Number Palindrome
+//This Java program prints the following pattern after being executed
 /*
  *       1
  *      212
@@ -13,13 +14,16 @@ public class PalindromicPattern {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i=1; i<=n; i++) {
+            //printing spaces
             int spaces = n-i;
             for (int j=1; j<=spaces; j++) {
                 System.out.print(" ");
             }
+            //printing left half triangle
             for (int j=i; j>=1; j--) {
                 System.out.print(j);
             }
+            //printing remaining triangle
             for (int j=2; j<=i; j++) {
                 System.out.print(j);
             }
