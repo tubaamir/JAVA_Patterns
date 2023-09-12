c492045327ef9268d85106ba7d129b7c143f70fb
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 09:59:49 +0530

    Add files via upload

---
+++ SimpleSquare.java
@@ -0,0 +1,25 @@
+//printing pattern rectangular block
+//This Java program prints the following pattern after being executed
+/*
+ *   * * * * *
+ *   * * * * *
+ *   * * * * *
+ */
+
+import java.util.Scanner;
+
+public class SimpleSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int m = sc.nextInt();
+        for (int i=1; i<=n; i++ ) {
+            for (int j=1; j<=m; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }
+        
+        sc.close();
+    }
+}
