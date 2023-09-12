f2c3006beb6488722fd93328fc66aafff241b7c8
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 10:02:32 +0530

    Update HollowRectangle.java

--- HollowRectangle.java
+++ HollowRectangle.java
@@ -1,4 +1,5 @@
 //printing pattern rectangular hollow block
+// This Java program prints the following pattern after being executed
 /*
  *   * * * * *
  *   *        *
@@ -11,10 +12,14 @@ import java.util.Scanner;
 public class HollowRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //input rows
         int n =sc.nextInt();
+        //input columns
         int m = sc.nextInt();
+        
         for (int i=1; i<=n; i++ ) {
             for (int j=1; j<=m; j++) {
+                //checking conditions for printing stars and spaces
                 if ( i == 1 || j == 1 || i == n || j == m ) {
                     System.out.print("*");
                 } else {
