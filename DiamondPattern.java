fd2a28f59a0e255ab98e392bde56fa35d67eab55
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 09:34:12 +0530

    Update DiamondPattern.java

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -1,4 +1,5 @@
 //Printing Pattern Number Pyramid
+//This Java program prints the following pattern after being executed
 /*
  *      *
  *     ***
@@ -14,11 +15,19 @@ public class DiamondPattern {
     public static void main (String args[] ) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        //for upper half
         for (int i=1; i<=n; i++) {
             int spaces = n-i;
+            //loop for printing spaces
             for (int j=1; j<=spaces; j++) {
                 System.out.print(" ");
             }
+            //loop for peinting upper traingle
+            /*
+                      *
+                     ***
+                    *****
+            */
             for (int j=i; j>=1; j--) {
                 System.out.print("*");
             }
@@ -28,12 +37,14 @@ public class DiamondPattern {
 
             System.out.println();
         }
-
+        // for lower part
         for (int i=n; i>=1; i--) {
             int spaces = n-i;
+            //printing spaces
             for (int j=1; j<=spaces; j++) {
                 System.out.print(" ");
             }
+            //print lower remaing triangle
             for (int j=i; j>=1; j--) {
                 System.out.print("*");
             }
