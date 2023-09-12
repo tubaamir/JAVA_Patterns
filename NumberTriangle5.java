6d4adde4da5ec8899416a3aa8c9a147b7bb3e157
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-09-12 10:08:58 +0530

    Update NumberTriangle5.java

--- NumberTriangle5.java
+++ NumberTriangle5.java
@@ -1,4 +1,5 @@
 //printing pattern 0-1 triangle
+// This Java program prints the following pattern after being executed
 /*
  *    1
  *    0 1
@@ -11,12 +12,12 @@ import java.util.Scanner;
 public class NumberTriangle5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //int x = 1;
+        
         int n = sc.nextInt();
-        //int m = sc.nextInt();
+        
         for (int i=1; i<=n; i++ ) {
             for (int j=1; j<=i; j++) {
-                if ((i+j)%2==0) {
+                if ( ( i + j ) % 2 == 0) {
                     System.out.print("1"+" ");
                 } else {
                     System.out.print("0"+" ");
